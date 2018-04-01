1a8c8b8dd865fcdd4abe934810daa78bc75e036a
Jessi DiTocco <jessi.ditocco@gmail.com>
2018-04-01 12:53:49 -0400

    Solved lazy lemmings challenge

---
+++ lazy_lemmings.py
@@ -0,0 +1,34 @@
+def lazy_lemmings(num_holes, cafes):
+    """
+    >>> lazy_lemmings(6, [2, 4])
+    2
+
+    """
+
+    # Loop through each hole in the range of num holes
+    # For each hole, subtract the cafes from that hole and create a list of these numbers
+    # Take the minimum of this list
+    # Check to see whether this minimum is greater than the worst and if so, update it
+    # Return the worst distance
+
+    longest_distance = 0
+
+    for hole in range(num_holes):
+        distance = min([abs(hole - cafe) for cafe in cafes])
+
+        # if distance > longest_distance:
+        #     longest_distance = distance
+
+        longest_distance = max(longest_distance, distance)
+
+
+    return longest_distance
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "All tests passed!"
+
+
+
