66a0f4642783360c8e4d4fef7994dd259792d69f
Jessi DiTocco <jessi.ditocco@gmail.com>
2018-04-19 14:21:35 -0700

    Solved split a string algorithm

--- split_a_string.py
+++ split_a_string.py
@@ -15,57 +15,54 @@ def split(astring, splitter):
 
     """
 
-    # loop through the string using enumerate so we can keep track 
-    # keep track of start_of_word
-    # make an empty list 
-    # of value and index
-    # if value equal splitter, split the string from start of word
-    # and append it to the list
-    # to the index that you are one
-    # set start of word to the index you are on plus 1
 
+    # .find("sub") method in python take a substring
+    # .find("sub", start, end) -- takes a start and end argument-- start is incluive
+    # end is not inclusive
+    # If the substring exitst inside of the string, it returns the lowest index
+    # where the substring is found
+    # if the substring doesn't exist, it returns -1
 
-    # splits = []
-    # start = 0
+    # initialize an empty list called split
+    # keep track of the start index --> starting at 0
+    # while start index is less than the length of the list:
+    # use find, searching like astring.find("splitter", start)
+        # this will return the lowest index of the found value
+        # splice the list from start to this index and add it to the list
+        # update start to split on plus 1
+    # return the split list
 
-    # for i, char in enumerate(astring):
-    #     if char == splitter:
-    #         splits.append(astring[start:i])
+    # out = []
+    # index = 0
 
-    #         start = i + 1
+    # while index <= len(astring):
+    #     curr_index = index
+    #     index = astring.find(splitter, index)
 
-    # return splits
+    #     if index != -1:
+    #         out.append(astring[curr_index:index])
+    #         index += len(splitter)
+    #     else:
+    #         # couldn't find any more instances of splitter in astring
+    #         out.append(astring[curr_index:])
+    #         break 
 
-
-
-
-
-
-
-    # keep track of start
-    # intialize an empty list
-    # loop through the indices and letters using enumerate
-    # if the charecter that you are on is equal to the spliter
-    # slice from start to that index and append it to the list
-    # update start to that new indice + 1
-
-    start = 0
-
-    split = []
-
-    for i, char in enumerate(astring):
-        print i, char
-        if char == splitter:
-            print astring[start:i]
-            split.append(astring[start:i])
-            start = i + 1
-
-    return split
-
-split("i love balloonicorn", " ")
+    # return out
 
 
 
+    # we need to keep track of our split list
+    # we need to keep track of the index that we are on
+
+    # while index is less than the length of the list
+    # we want to keep track of the current index
+    # we also want to reset index to the astring.find(splitter, index)
+    # then, we want to check that the index returned is not -1 (which would mean there are no instances of the splitter)
+    # if the index returned is not negative 1, we want to slice and append from curr index
+    # to the index of the splitter
+    # then we will increment the index by the length of the splitter
+    # if the index of find comes out to -1, this means that there are no more 
+    # instances of splitter so we will append the string from the current index to the end of the string
 
 
 
