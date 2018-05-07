6d7c4050fc1b61848f0ebd50661d2eeae00cf1b0
Jessi DiTocco <jessi.ditocco@gmail.com>
2018-05-07 14:18:13 -0700

    Worked on using AJAX calls for get and post

--- ajax.js
+++ ajax.js
@@ -31,3 +31,56 @@ function updateStatus () {
 $("#update-status").on("click", updateStatus);
 
 
+//////////////////// Javascript with AJAX-- Post/////////////////////////
+
+function showOrderResults(result) {
+    alert(result);
+} 
+
+function submitOrder(evt) {
+    evt.preventDefault();
+
+    let formInputs = {
+        "type": $("#type-field").val(), 
+        "amount" $("#amount-field").val(),
+    };
+
+    $.post("/new-order", formInputs, showOrderResults);
+
+}
+
+$("#order-form").on("submit", submitOrder);
+
+//////////////////// Delivery Time /////////////////////////
+
+function showDeliveryInfo(data) {
+    let days = data["days"];
+    let cost = data["cost"];
+    alert('This will arive in' + days + "days");
+    alert('This will cost' + cost);
+}
+
+function getDeliveryInfo(evt) {
+    evt.preventDefault();
+    let formValues = $("#color-form").serialize();
+
+    $.get("/deliver-info.json", formValues, showDeliveryInfo);
+}
+
+$("#delivery-form").on("submit", getDeliveryInfo);
+
+//////////////////// You can also write AJAX with inline functions ///////////////////
+
+function showInfo(results) {
+    alert("Melon type:" + results);
+}
+
+function getMelonInfo() {
+    $.get("/melon-info", showInfo);
+}
+
+function getMelonInfo() {
+    $.get("/melon-info", function(results) {
+       alert("Melon type:" + results);
+    });
+}
